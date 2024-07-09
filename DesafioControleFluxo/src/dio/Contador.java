e1cbc459ee51fba14b9e52a24b5a390c22a8812d
breniru <breno10sp14@gmail.com>
2024-07-09 01:35:08 -0300

    desafio concluido(descricao do desadio na classe Contador.java)

---
+++ DesafioControleFluxo/src/dio/Contador.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package dio;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author breno
+ */
+public class Contador {
+	public static void main(String[] args){
+		Scanner terminal = new Scanner(System.in);
+		System.out.println("Digite o primeiro parâmetro");
+		int parametroUm = terminal.nextInt();
+		System.out.println("Digite o segundo parâmetro");
+		int parametroDois = terminal.nextInt();
+		
+		try {
+			//chamando o método contendo a lógica de contagem
+			contar(parametroUm, parametroDois);
+		
+		}catch (ParametrosInvalidosException exception) {
+                    System.out.println(exception.getMessage());
+		}
+		
+	}
+	static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
+		//validar se parametroUm é MAIOR que parametroDois e lançar a exceção
+                if(parametroUm > parametroDois){
+                    throw new ParametrosInvalidosException("O segundo parâmetro deve ser maior que o primeiro");
+                }
+		
+		int contagem = parametroDois - parametroUm;
+		//realizar o for para imprimir os números com base na variável contagem
+                
+                for(int i = 1; i <= contagem; i++){
+                    System.out.println(i);
+                }
+                
+	}
+        
+        
+        
+}
